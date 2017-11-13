75ca849b86380314213f12e6910a632fe8ec789a
Tyrone McElrath <tmcelrat@kent.edu>
2017-11-13 17:12:14 -0500

    init commit

---
+++ Scripts/Script.js
@@ -0,0 +1,15 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+function noDrop(ev){
+	return false;
+}
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
